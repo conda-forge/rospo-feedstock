8ec00b0972cb5cce46fa41b9b1ee885129872d58
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-09 08:56:55 -0700

    add platforms (#1)
    * add platforms
    
    * use azure for aarch64
    
    * fix Apple Silicon

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X 'github.com/ferama/rospo/cmd.Version=${PKG_VERSION}'"
 go-licenses save . --save_path=license-files
 
-mkdir -p ${PREFIX}/etc/bash_completion.d
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d
-mkdir -p ${PREFIX}/share/zsh/site-functions
-rospo completion bash > ${PREFIX}/etc/bash_completion.d/rospo
-rospo completion fish > ${PREFIX}/share/fish/vendor_completions.d/rospo.fish
-rospo completion zsh > ${PREFIX}/share/zsh/site-functions/_rospo
+if [[ ${build_platform} == ${target_platform} ]]; then
+    mkdir -p ${PREFIX}/etc/bash_completion.d
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    rospo completion bash > ${PREFIX}/etc/bash_completion.d/rospo
+    rospo completion fish > ${PREFIX}/share/fish/vendor_completions.d/rospo.fish
+    rospo completion zsh > ${PREFIX}/share/zsh/site-functions/_rospo
+fi
